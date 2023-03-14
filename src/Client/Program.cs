ae96ece6e666742ac5e4129e32cb34bb1f6d7942
reeditor <57895659+reeditor-git@users.noreply.github.com>
2023-03-14 22:05:25 +0200

    installed radzen blazor

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using Replica.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 await builder.Build().RunAsync();
