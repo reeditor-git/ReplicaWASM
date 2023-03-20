eed8cb0f5b60e405db174583276790340e8b2ef1
reeditor <57895659+reeditor-git@users.noreply.github.com>
2023-03-20 18:29:44 +0200

    added site logo, changed navigation, added syncfusion license

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Replica.Client;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSyncfusionBlazor();
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTM3OTIwOUAzMjMwMmUzNDJlMzBHWnV6OWRYb1dZdHJLd1VvQ1FXeTUxZlRtdFJhZ2hEdStqcGEveTRkNmxBPQ==");
 
 await builder.Build().RunAsync();
